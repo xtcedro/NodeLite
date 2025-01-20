35d26dd71b735b5c62243bb969a6f75d2795e78a
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-20 13:11:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../html/public')));
 
 // API Routes
 app.use('/api/appointments', appointmentRoutes);
