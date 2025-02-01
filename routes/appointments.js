9571586df836b20a971f9e36602c8ca9e6711a2c
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-31 20:20:42 -0600

    Update appointments.js

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { submitAppointment, fetchAppointments } from "../controllers/appointmentController.js";
+import { submitAppointment, fetchAppointments } from "../controllers/appointmentsController.js";
 
 const router = express.Router();
 
+// Appointment submission route
 router.post("/", submitAppointment);
+
+// Fetch all appointments route
 router.get("/", fetchAppointments);
 
 export default router;
