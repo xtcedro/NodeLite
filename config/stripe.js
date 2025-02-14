200f2707d06a333155897387b102a8d4dc6647a7
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-02-14 10:31:41 -0600

    Update stripe.js

--- config/stripe.js
+++ config/stripe.js
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 dotenv.config(); // Load environment variables
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2025-01-27', // Ensure correct Stripe API version
+    apiVersion: '2025-01-27.acacia', // Ensure correct Stripe API version
 });
 
 export default stripe;
